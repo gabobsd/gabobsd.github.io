c61631439c8247102d02bbd782a6d885a89c60da
Gabo Tse <GaboTse@Gabos-MacBook-Pro.local>
2017-03-29 15:30:06 +0800

    testing hammer.js

--- jquerymobile/gameplay.js
+++ jquerymobile/gameplay.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
+
 let score = 0;
+let points = document.getElementById('points');
+const test = document.getElementById('test');
+
+test.on('doubletap', function() {
+	alert('test');
+});
+
+
+
+});//closing for doc.ready
