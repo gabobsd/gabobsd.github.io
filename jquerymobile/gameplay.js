75edaea6e24362851bc50c7873576c28b7f13cea
Gabo Tse <GaboTse@Gabos-MacBook-Pro.local>
2017-03-30 12:07:56 +0800

    added reset button functionality

--- jquerymobile/gameplay.js
+++ jquerymobile/gameplay.js
@@ -4,10 +4,11 @@
 //ROCK PAPER SCISSORS
 $(document).on("pagecreate","#rockpaperscissors",function(event){
   // alert("pagecreate event fired!")
-
-let playerScore = document.getElementById('rps-points');
-let compScore = document.getElementById('rps-comp-points');
-let reset = document.getElementById('rps-reset-button');
+let playerScore = 0;
+let compScore = 0;
+let playerPoints = $('#rps-points');
+let compPoints = $('#rps-comp-points');
+let reset = $('#rps-reset-button');
 // const rock = document.getElementById('rock');
 // const paper = document.getElementById('paper');
 // const scissors = document.getElementById('scissors');
@@ -15,6 +16,23 @@ let reset = document.getElementById('rps-reset-button');
 $('#rock').on("tap",function(){
     alert('test');
   }); 
+
+$('#paper').on("tap",function(){
+    alert('test');
+  }); 
+
+$('#scissors').on("tap",function(){
+    alert('test');
+  }); 
+
+$(reset).on("tap",function(){
+    alert('test');
+    playerScore = 0;
+    compScore = 0;
+    $(playerPoints).html(playerScore);
+    $(compPoints).html(compScore);
+  }); 
+
 
 // rock.addEventListener('click', function(){
 // alert('test');
