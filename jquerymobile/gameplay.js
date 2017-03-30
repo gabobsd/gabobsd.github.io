77c980eb2e8e733ac195dbdc288ef16418bd5a40
Gabo Tse <GaboTse@Gabos-MacBook-Pro.local>
2017-03-30 11:39:53 +0800

    testing alerts

--- jquerymobile/gameplay.js
+++ jquerymobile/gameplay.js
@@ -4,9 +4,19 @@ let score = 0;
 let points = document.getElementById('points');
 const test = document.getElementById('test');
 
-test.on('doubletap', function() {
-	alert('test');
+
+//ROCK PAPER SCISSORS
+let playerScore = document.getElementById('rps-points');
+let compScore = document.getElementById('rps-comp-points');
+let reset = document.getElementById('rps-reset-button');
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+
+rock.addEventListener('click', function(){
+alert('test');
 });
+
 
 
 
