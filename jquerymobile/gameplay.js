03d85bfb607bbeea44911770b3986bce6da53ace
Gabo Tse <GaboTse@Gabos-MacBook-Pro.local>
2017-03-30 12:23:59 +0800

    added functionality for rock in RPS

--- jquerymobile/gameplay.js
+++ jquerymobile/gameplay.js
@@ -6,12 +6,37 @@ let compScore = 0;
 let playerPoints = $('#rps-points');
 let compPoints = $('#rps-comp-points');
 let reset = $('#rps-reset-button');
+// let resultText = $('#resultText');
 // const rock = document.getElementById('rock');
 // const paper = document.getElementById('paper');
 // const scissors = document.getElementById('scissors');
 
+//Randomize computer choices
+function getComputerChoice() {
+  let choices = ['rock', 'paper', 'scissors'];
+  let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  return computerChoice;
+}
+
+
 $('#rock').on("tap",function(){
-    alert('test');
+  let computerChoose = getComputerChoice();
+  if (computerChoose === "scissors") {
+    $(resultText).text("You played Rock. Computer played Scissors. You win!");
+    playerScore++;
+    $(playerPoints).text(playerScore);
+  }
+
+  else if (computerChoose === "paper") {
+    $(resultText).text("You played Rock. Computer played Paper. Computer wins!");
+    compScore++;
+    $(compPoints).text(compScore);
+  }
+
+  else {
+  	 $(resultText).text("You played Rock. Computer played Rock. It's a tie!");
+  }
+
   }); 
 
 $('#paper').on("tap",function(){
@@ -26,7 +51,7 @@ $(reset).on("tap",function(){
     alert('test');
     playerScore = 0;
     compScore = 0;
-    $(playerPoints).html(playerScore);
-    $(compPoints).html(compScore);
+    $(playerPoints).text(playerScore);
+    $(compPoints).text(compScore);
   }); 
 });
